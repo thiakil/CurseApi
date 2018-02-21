c71a3ed0ab6cb4b3b7f6c5f9b1e06ff419fda5c1
Xander <x@xandervictory.id.au>
2018-02-21 22:10:52 +0800

    add gson annotations in case fields get renamed

--- src/main/java/com/thiakil/curseapi/login/CurseLoginResponse.java
+++ src/main/java/com/thiakil/curseapi/login/CurseLoginResponse.java
@@ -1,29 +1,50 @@
 package com.thiakil.curseapi.login;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Thiakil on 21/02/2018.
  */
 public class CurseLoginResponse {
+	@SerializedName("Status")
 	public int Status;
+	@SerializedName("StatusMessage")
 	public String StatusMessage;
+	@SerializedName("Session")
 	public LoginSession Session;
+	@SerializedName("Timestamp")
 	public long Timestamp;
+	@SerializedName("TwitchUsernameReservationToken")
 	public String TwitchUsernameReservationToken;
-	
+
 	public static class LoginSession{
+		@SerializedName("UserID")
 		public long UserID;
+		@SerializedName("Username")
 		public String Username;
+		@SerializedName("DisplayName")
 		public String DisplayName;
+		@SerializedName("SessionID")
 		public String SessionID;
+		@SerializedName("Token")
 		public String Token;
+		@SerializedName("EmailAddress")
 		public String EmailAddress;
+		@SerializedName("EffectivePremiumStatus")
 		public boolean EffectivePremiumStatus;
+		@SerializedName("ActualPremiumStatus")
 		public boolean ActualPremiumStatus;
+		@SerializedName("SubscriptionToken")
 		public long SubscriptionToken;
+		@SerializedName("Expires")
 		public long Expires;
+		@SerializedName("RenewAfter")
 		public long RenewAfter;
+		@SerializedName("IsTemporaryAccount")
 		public boolean IsTemporaryAccount;
+		@SerializedName("IsMerged")
 		public boolean IsMerged;
+		@SerializedName("Bans")
 		public int Bans;
 	}
 }
