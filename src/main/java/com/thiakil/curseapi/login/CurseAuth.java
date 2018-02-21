a6c4add395cba5d480760c89b14a10943a51fcd9
Xander <x@xandervictory.id.au>
2018-02-21 22:11:09 +0800

    make JsonObjects directly

--- src/main/java/com/thiakil/curseapi/login/CurseAuth.java
+++ src/main/java/com/thiakil/curseapi/login/CurseAuth.java
@@ -1,6 +1,7 @@
 package com.thiakil.curseapi.login;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -22,9 +23,9 @@ public class CurseAuth {
 
 	public static CurseLoginResponse getSessionFromCurseAccount(String username, String password) throws CurseAuthException{
 		HttpPost post = new HttpPost("https://logins-v1.curseapp.net/login");
-		Map<String,String> data = new HashMap<>();
-		data.put("username", username);
-		data.put("password", password);
+		JsonObject data = new JsonObject();
+		data.addProperty("username", username);
+		data.addProperty("password", password);
 		post.setEntity(new JsonEntity(data));
 		try {
 			CloseableHttpResponse response = httpclient.execute(post);
@@ -43,6 +44,9 @@ public class CurseAuth {
 
 	public static CurseToken getTokenFromCurseAccount(String username, String password) throws CurseAuthException{
 		CurseLoginResponse response = getSessionFromCurseAccount(username, password);
+		if (response.Status != LoginStatus.Success){
+			throw new CurseAuthException("Unknown login status: "+response.Status);
+		}
 		return new CurseToken(response.Session.UserID, response.Session.Token);
 	}
 
--- src/main/java/com/thiakil/curseapi/login/JsonEntity.java
+++ src/main/java/com/thiakil/curseapi/login/JsonEntity.java
@@ -1,6 +1,7 @@
 package com.thiakil.curseapi.login;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 
@@ -14,5 +15,9 @@ public class JsonEntity extends StringEntity {
 
 	public JsonEntity(Map keys){
 		super(GSON.toJson(keys), ContentType.APPLICATION_JSON);
+	}
+
+	public JsonEntity(JsonObject obj){
+		super(GSON.toJson(obj), ContentType.APPLICATION_JSON);
 	}
 }
