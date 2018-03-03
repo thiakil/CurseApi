3cab1554a7ba7338d08b68552748979088ad3d34
Xander <x@xandervictory.id.au>
2018-03-03 12:58:31 +0800

    loginsession helpers, incl auto renew

--- src/main/java/com/thiakil/curseapi/login/LoginSession.java
+++ src/main/java/com/thiakil/curseapi/login/LoginSession.java
@@ -33,44 +33,135 @@ package com.thiakil.curseapi.login;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class LoginSession {
 	@SerializedName("UserID")
 	public long userID;
+
 	@SerializedName("Username")
 	public String username;
+
 	@SerializedName("DisplayName")
 	public String displayName;
+
 	@SerializedName("SessionID")
 	public String sessionID;
+
 	@SerializedName("Token")
 	public String token;
+
 	@SerializedName("EmailAddress")
 	public String emailAddress;
+
 	@SerializedName("EffectivePremiumStatus")
 	public boolean effectivePremiumStatus;
+
 	@SerializedName("ActualPremiumStatus")
 	public boolean actualPremiumStatus;
+
 	@SerializedName("SubscriptionToken")
 	public long subscriptionToken;
+
 	@SerializedName("Expires")
 	public long expires;
+
 	@SerializedName("RenewAfter")
 	public long renewAfter;
+
 	@SerializedName("IsTemporaryAccount")
 	public boolean isTemporaryAccount;
+
 	@SerializedName("IsMerged")
 	public boolean isMerged;
+
 	@SerializedName("Bans")
 	public UserBanType bans;
 
+	/**
+	 * Helper to create a CurseToken from this session's info
+	 * @return a curseToken
+	 */
 	public CurseToken createCurseToken(){
 		return new CurseToken(userID, token);
 	}
 
+	/**
+	 * Attempts to renew this session, and updates the expiry info on success.
+	 * @throws CurseAuthException if any failure, in which case the token was likely not renewed.
+	 */
 	public void renew() throws CurseAuthException{
 		RenewTokenResponse response = CurseAuth.renewAccessToken(token);
 		expires = response.expires;
 		renewAfter = response.renewAfter;
+	}
+
+	/**
+	 * Test if the token has expired
+	 * @return true if it is ok to use, false if it has expired.
+	 */
+	public boolean isValid(){
+		return expires > System.currentTimeMillis();
+	}
+
+	/**
+	 * Tests if the session is within the Curse recommended renew window.
+	 * @return true if the token should be renewed in order to stay valid.
+	 */
+	public boolean shouldRenew(){
+		return renewAfter <= System.currentTimeMillis();
+	}
+
+	/**
+	 * Convenience method to check the time left on this token
+	 * @return milliseconds until this session will expire
+	 */
+	public long millisUntilExpiry(){
+		return expires - System.currentTimeMillis();
+	}
+
+	/**
+	 * Sets up auto session renewal, errors will be printed on stderr.
+	 */
+	public void autoRenew(){
+		scheduleRenew(this);
+	}
+
+	private static ScheduledExecutorService scheduler;
+
+	private static void scheduleRenew(LoginSession loginSession){
+		if (!loginSession.isValid()){
+			throw new IllegalArgumentException("invalid LoginSession; already expired");
+		}
+		scheduleRenew(new SessionRenewer(loginSession));
+	}
+
+	private static void scheduleRenew(SessionRenewer renewer){
+		if (scheduler == null){
+			scheduler = Executors.newScheduledThreadPool(1);
+		}
+		scheduler.schedule(renewer, renewer.loginSession.millisUntilExpiry(), TimeUnit.MILLISECONDS);
+	}
+
+	private static class SessionRenewer implements Runnable{
+		final LoginSession loginSession;
+
+		SessionRenewer(LoginSession loginSession) {
+			this.loginSession = loginSession;
+		}
+
+		@Override
+		public void run() {
+			try {
+				loginSession.renew();
+				scheduleRenew(this);
+			} catch (CurseAuthException e){
+				System.err.println("Failed to renew token for "+loginSession.username);
+				System.err.println(e.getMessage());
+			}
+		}
 	}
 
 	public enum UserBanType
